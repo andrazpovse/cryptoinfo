1596753c6873edb820c2e13e05374d249d356f39
Andraz <andraz.povse1@gmail.com>
2017-03-31 16:00:55 +0200

    changed so there are much less API calls. Gets first 100 coins, then finds the ones you need.

--- cryptoinfo.py
+++ cryptoinfo.py
@@ -1,23 +1,41 @@
 import urllib, json
 
-#function getPriceOfCoin returns the price of the passed (with API url) coin in USD
-def getPriceOfCoin(coinAPI):
-   response = urllib.urlopen(coinAPI)
-   data = json.loads(response.read())[0]
-   priceUSD = "$" + data['price_usd'] 
-   return priceUSD
 
-#add coin API for coins you would like to get information on
-iconomi = "https://api.coinmarketcap.com/v1/ticker/iconomi/"
-lbry = "https://api.coinmarketcap.com/v1/ticker/library-credit/"
-stratis = "https://api.coinmarketcap.com/v1/ticker/stratis/"
+#Coinmarketcap limit API to 10 GET requests per 1 minute
 
 
-#calls for price in USD of a specific coin
-priceUSD = getPriceOfCoin(lbry)
+#FUNCTIONS OMMITED BECAUSE THEY WOULD GENERATE TOO MUCH API REQUESTS
 
 
-print priceUSD
+#gives you first 100 coins...coin one is at index [0] and so on to [99]
+#dont know how they are sorted yet
+def getFirst100CoinsData():
+    response = urllib.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=100")
+    data = json.loads(response.read())
+    return data
+
+#get price of coin.
+#data....JSON object with coins
+#coinName....name of the coin (e.g. Bitcoin, Monero, Steem, LBRY Credits)
+#coinName also works with symbols (e.g. BTC, ICN, ETH)
+def getPriceOfCoin(data, coinName):
+    for i in range(0, len(data)):
+        if(data[i]['name'] == coinName) or (data[i]['symbol'] == coinName):
+            return data[i]['name'] + '  $'+data[i]['price_usd']
+
+
+
+#not needed, since we pull first 100 coins...
+#API get for first 100 coins
+data = getFirst100CoinsData()
+
+#Put the coins you want information on in a list
+listOfCoins = ['ICN', 'LBC', 'Steem']
+
+#goes through list, displays price of coins in the list
+for i in listOfCoins:
+	print getPriceOfCoin(data, i)
+
 
 
 
