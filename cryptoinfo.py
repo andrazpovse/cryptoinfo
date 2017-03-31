2c509adf2f8dd228067b434dcb0b80972f2a62b7
Andraz <andraz.povse1@gmail.com>
2017-03-31 23:25:23 +0200

    added getSomeInfo function

--- cryptoinfo.py
+++ cryptoinfo.py
@@ -1,13 +1,11 @@
 import urllib, json
 
-
 #Coinmarketcap limit API to 10 GET requests per 1 minute
 
 
-#FUNCTIONS OMMITED BECAUSE THEY WOULD GENERATE TOO MUCH API REQUESTS
 
 
-#gives you first 100 coins...coin one is at index [0] and so on to [99]
+#API GET gives you first 100 coins...coin one is at index [0] and so on to [99]
 #dont know how they are sorted yet
 def getFirst100CoinsData():
     response = urllib.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=100")
@@ -22,19 +20,37 @@ def getPriceOfCoin(data, coinName):
     for i in range(0, len(data)):
         if(data[i]['name'] == coinName) or (data[i]['symbol'] == coinName):
             return data[i]['name'] + '  $'+data[i]['price_usd']
+#percent_change_1h
+def getPercentChange24h(data, coinName):
+    for i in range(0, len(data)):
+    	if(data[i]['name'] == coinName) or (data[i]['symbol'] == coinName):
+            return data[i]['name']+data[i]['percent_change_1h']+' %'
+
+#gets you some info on the coin...price, 24h change, 1h change
+def getSomeInfo(data, coinName):
+	for i in range(0, len(data)):
+		if(data[i]['name'] == coinName) or (data[i]['symbol'] == coinName):
+			return data[i]['name'] + '\nPrice: $' + data[i]['price_usd'] + '\n1h change: '+ data[i]['percent_change_1h']+'%\n24h change: ' + data[i]['percent_change_24h'] + '%\n'
 
 
-
-#not needed, since we pull first 100 coins...
-#API get for first 100 coins
 data = getFirst100CoinsData()
 
 #Put the coins you want information on in a list
 listOfCoins = ['ICN', 'LBC', 'Steem']
 
+
+
+# TO DO
+""" 
+timer function, so the data would print itself every 5 minutes
+GUI
+Connect with database, to display charts, maybe predict
+"""
+
+
 #goes through list, displays price of coins in the list
 for i in listOfCoins:
-	print getPriceOfCoin(data, i)
+	print getSomeInfo(data, i)
 
 
 
