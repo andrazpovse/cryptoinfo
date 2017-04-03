5d3c64e69be98afb079a53bad6989f32591b7850
Andraz <andraz.povse1@gmail.com>
2017-04-03 21:04:16 +0200

    coloured output in cmd line

--- chatBoxWAMP.py
+++ chatBoxWAMP.py
@@ -2,6 +2,8 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 
 
+def tamperMessage(message):
+	print message.split()[0]
 
 
 
@@ -12,7 +14,7 @@ class MyComponent(ApplicationSession):
         print("session joined")
         
         def gotMessage(type, messageNumber, username, message, reputation):
-        	print message
+        	tamperMessage(message)
         	
         # 1. subscribe to a topic so we receive events
         try:
--- chatScraper.py
+++ chatScraper.py
@@ -4,8 +4,19 @@ from collections import Counter
 from bs4 import BeautifulSoup
 from HTMLParser import HTMLParser
 
+global stayAndWait
+stayAndWait = 45#in seconds...how long to stay on the site...the longer, the more messages
 
-stayAndWait = 30#in seconds...how long to stay on the site...the longer, the more messages
+global worthlessWords
+worthlessWords = ['just','what','will','or','that','for','the', 'i', 'and', 'is', 'in', 'on', 'you', 'a', 'your', 'it', 'this', 'my', 'to']
+
+
+#list of top 100 coins
+global usefulCoinWords
+usefulCoinWords = ['BTC','Bitcoin','ETH','Ethereum','XRP','Ripple','DASH','Dash','LTC','Litecoin','XMR','Monero','ETC','Ethereum Classic','XEM','NEM','REP','Augur','MAID','MaidSafeCoin','GNT','Golem','ZEC','Zcash','DOGE','Dogecoin','USDT','Tether','DCR','Decred','ICN','Iconomi','PIVX','PIVX','STEEM','Steem','DGD','DigixDAO','WAVES','Waves','FCT','Factom','STRAT','Stratis','LSK','Lisk','BTS','BitShares','1ST','First Blood','XLM','Stellar Lumens','ROUND','Round','BCC','BitConnect','BCN','Bytecoin','SNGLS','SingularDTV','ARDR','Ardor','MLN','Melon','GAME','GameCredits','KMD','Komodo','PPC','Peercoin','NXT','Nxt','EMC','Emercoin','SC','Siacoin','NXS','Nexus','XCP','Counterparty','SJCX','Storjcoin X','NMC','Namecoin','SDC','ShadowCash','ANS','AntShares','CRBIT','Creditbit','BTCD','BitcoinDark','NLG','Gulden','BCY','Bitcrystals','XZC','ZCoin','GBYTE','Byteball','XAUR','Xaurum','SYS','SysCoin','IOC','I/O Coin','AMP','Synereo','YBC','YbCoin','BLK','BlackCoin','UBQ','Ubiq','RBY','Rubycoin','AGRS','Agoras Tokens','TIME','Chronobank','NAV','NAV Coin','POT','PotCoin','ARK','Ark','ION','ION','SLR','SolarCoin','NVC','Novacoin','GRC','GridCoin','CRW','Crown','NXC','Nexium','RADS','Radium','XPM','Primecoin','BAY','BitBay','VPN','VPNCoin','UNITY','SuperNET','PEPECASH','Pepe Cash','DGB','DigiByte','EXP','Expanse','XBC','Bitcoin Plus','OMNI','Omni','LBC','LBRY Credits','LMC','LoMoCoin','EDR','E-Dinar Coin','BELA','BelaCoin','CLAM','Clams','XBB','Boolberry','NAUT','NautilusCoin','AEON','Aeon','MUE','MonetaryUnit','MONA','MonaCoin','BURST','Burst','CURE','CureCoin','PASC','Pascal Coin','VTC','Vertcoin','ZCL','ZClassic','JINN','Jinn','ARC','ArcticCoin','VSL','vSlice','VIA','Viacoin','HEAT','HEAT','FAIR','FairCoin']
+
+
+
 
 #strips tags from HTMl
 #http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
@@ -22,59 +33,16 @@ def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
-
-
-
-session = dryscrape.Session()
-print "establishing connection..."
-session.visit("https://poloniex.com/exchange")
-
-for i in range(1,stayAndWait):
-	print str(i)+"/"+str(stayAndWait)+" seconds passed"
-	time.sleep(1)
-
-response = session.body()
-
-soup = BeautifulSoup(response, "lxml")
-result = soup.find(id="trollbox") #trollboxtable messages
-
-text = str(result) #turn beautifulsoup into a string
-messages = strip_tags(text) #strip only messages from html
-#at this point, the messages are still in tact.....
-#get sentence meaning here !!!-------------------------------------------------------------------------------------------------------------
-
-
-
-#split messages by space
-messages = messages.split()
-
-#convert messages to lowercase
-for i in range(0,len(messages)):
-	messages[i] = messages[i].lower()
-
-
-
-#we get words and counter, occurencies of the word
-wordAndCtr = Counter(messages).most_common() 
-#we get a list of tuples, ("word", times)
-#must eliminate worthless words like "the", "and", ...
-worthlessWords = ['just','what','will','or','that','for','the', 'i', 'and', 'is', 'in', 'on', 'you', 'a', 'your', 'it', 'this', 'my', 'to']
-
-
-usefulCoinWords = ['BTC','Bitcoin','ETH','Ethereum','XRP','Ripple','DASH','Dash','LTC','Litecoin','XMR','Monero','ETC','Ethereum Classic','XEM','NEM','REP','Augur','MAID','MaidSafeCoin','GNT','Golem','ZEC','Zcash','DOGE','Dogecoin','USDT','Tether','DCR','Decred','ICN','Iconomi','PIVX','PIVX','STEEM','Steem','DGD','DigixDAO','WAVES','Waves','FCT','Factom','STRAT','Stratis','LSK','Lisk','BTS','BitShares','1ST','First Blood','XLM','Stellar Lumens','ROUND','Round','BCC','BitConnect','BCN','Bytecoin','SNGLS','SingularDTV','ARDR','Ardor','MLN','Melon','GAME','GameCredits','KMD','Komodo','PPC','Peercoin','NXT','Nxt','EMC','Emercoin','SC','Siacoin','NXS','Nexus','XCP','Counterparty','SJCX','Storjcoin X','NMC','Namecoin','SDC','ShadowCash','ANS','AntShares','CRBIT','Creditbit','BTCD','BitcoinDark','NLG','Gulden','BCY','Bitcrystals','XZC','ZCoin','GBYTE','Byteball','XAUR','Xaurum','SYS','SysCoin','IOC','I/O Coin','AMP','Synereo','YBC','YbCoin','BLK','BlackCoin','UBQ','Ubiq','RBY','Rubycoin','AGRS','Agoras Tokens','TIME','Chronobank','NAV','NAV Coin','POT','PotCoin','ARK','Ark','ION','ION','SLR','SolarCoin','NVC','Novacoin','GRC','GridCoin','CRW','Crown','NXC','Nexium','RADS','Radium','XPM','Primecoin','BAY','BitBay','VPN','VPNCoin','UNITY','SuperNET','PEPECASH','Pepe Cash','DGB','DigiByte','EXP','Expanse','XBC','Bitcoin Plus','OMNI','Omni','LBC','LBRY Credits','LMC','LoMoCoin','EDR','E-Dinar Coin','BELA','BelaCoin','CLAM','Clams','XBB','Boolberry','NAUT','NautilusCoin','AEON','Aeon','MUE','MonetaryUnit','MONA','MonaCoin','BURST','Burst','CURE','CureCoin','PASC','Pascal Coin','VTC','Vertcoin','ZCL','ZClassic','JINN','Jinn','ARC','ArcticCoin','VSL','vSlice','VIA','Viacoin','HEAT','HEAT','FAIR','FairCoin']
-
-
-
 #get the trend based on text....integer
 def downTrend(text):
-	trendDOWN = ['down', 'drop', 'fall', 'sell']
+	trendDOWN = ['down', 'drop', 'fall', 'sell', 'dead']
 	counter = 0;
 	for i in text:
 		for j in trendDOWN:
 			if (i.lower() == j.lower()):
 				counter+=1;
 
-	return "Down trend is: " + str(counter)
+	return counter
 
 
 #get the trend based on text....integer
@@ -86,7 +54,7 @@ def upTrend(text):
 			if (i.lower() == j.lower()):
 				counter+=1;
 
-	return "Up trend is: " + str(counter)
+	return counter
 
 
 #is the word worthless....if it is, the functon will return True, else False
@@ -102,11 +70,63 @@ def isTheWordACoin(word, coins):
 		if (i.lower() == word.lower()): #comparison is in lower case
 			return True
 	return False
-#ce je IF stavek true tuple obrdzimo, ce je false, ga odstranimo
-coinMentions = [i for i in wordAndCtr if(isTheWordACoin(i[0], usefulCoinWords) == True)]
-eliminatedWords = [i for i in wordAndCtr if(isTheWordWorthless(i[0], worthlessWords) == False)]
 
-print coinMentions
-print upTrend(messages)
-print downTrend(messages)
 
+
+
+def mainFunction():
+	session = dryscrape.Session()
+	print "establishing the connection..."
+	session.visit("https://poloniex.com/exchange")
+
+	for i in range(1,stayAndWait):
+		print str(i)+"/"+str(stayAndWait)+" seconds passed"
+		time.sleep(1)
+
+	response = session.body()
+
+	soup = BeautifulSoup(response, "lxml")
+	result = soup.find(id="trollbox") #trollboxtable messages
+
+	text = str(result) #turn beautifulsoup into a string
+	messages = strip_tags(text) #strip only messages from html
+	#at this point, the messages are still in tact.....
+	#get sentence meaning here !!!-------------------------------------------------------------------------------------------------------------
+
+
+
+	#split messages by space
+	messages = messages.split()
+
+	#convert messages to lowercase
+	for i in range(0,len(messages)):
+		messages[i] = messages[i].lower()
+
+
+
+	#we get words and countser, occurencies of the word
+	wordAndCtr = Counter(messages).most_common() 
+	#we get a list of tuples, ("word", times)
+	#must eliminate worthless words like "the", "and", ...
+
+
+
+	#ce je IF stavek true tuple obrdzimo, ce je false, ga odstranimo
+	coinMentions = [i for i in wordAndCtr if(isTheWordACoin(i[0], usefulCoinWords) == True)]
+	eliminatedWords = [i for i in wordAndCtr if(isTheWordWorthless(i[0], worthlessWords) == False)]
+
+	print ('\x1b[0;37;44m' + str(coinMentions) + '\x1b[0m')
+	up=""
+	down=""
+	for i in range(0,upTrend(messages)):
+		up += "+"
+	for i in range(0, downTrend(messages)):
+		down += "+"
+
+	print("Up trend:   "+'\x1b[6;30;42m' + up + " ("+ str(len(up))+ ")"+'\x1b[0m')
+	print("Down trend: "+'\x1b[0;37;41m' + down + " (" + str(len(down)) + ")"+'\x1b[0m')
+
+	mainFunction()
+
+
+mainFunction()
