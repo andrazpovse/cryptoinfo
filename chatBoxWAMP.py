6fd222d8f980c7fbaf6abb420bf036229619b971
Andraz <andraz.povse1@gmail.com>
2017-04-03 10:44:36 +0200

    added a new way to get messages, WAMP, directly from Polo API...in the new file, 'chatBoxWAMP.py'

---
+++ chatBoxWAMP.py
@@ -0,0 +1,28 @@
+from twisted.internet.defer import inlineCallbacks
+from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+
+
+
+
+
+
+
+
+class MyComponent(ApplicationSession):
+
+    @inlineCallbacks
+    def onJoin(self, details):
+        print("session joined")
+        
+        def gotMessage(type, messageNumber, username, message, reputation):
+        	print message
+
+        # 1. subscribe to a topic so we receive events
+        try:
+        	yield self.subscribe(gotMessage,u'trollbox')
+        	print "subscribed"
+        except Exception as e:
+            print("could not subscribe to topic:")
+
+runner = ApplicationRunner(url=u"wss://api.poloniex.com", realm=u"realm1")
+runner.run(MyComponent)
